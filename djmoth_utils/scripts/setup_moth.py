f4a0377444950efdc84700c62f9c8db877eac583
Andres Riancho <andres.riancho@gmail.com>
2013-12-10 14:21:34 -0300

    Removed utils.py

--- djmoth_utils/scripts/setup_moth.py
+++ djmoth_utils/scripts/setup_moth.py
@@ -29,8 +29,11 @@ import logging
 import tempfile
 import subprocess
 
-from djmoth_utils.scripts.utils import configure_logging
-
+try:
+    from fabric.colors import red, yellow, green
+except ImportError:
+    # In case we don't have fabric
+    red = yellow = green = lambda x: x
 
 DJANGO_MOTH_REPO = 'https://github.com/andresriancho/django-moth.git'
 DJANGO_MOTH_DIR = 'django-moth'
@@ -39,6 +42,42 @@ ARTIFACTS_DIR = os.environ.get('CIRCLE_ARTIFACTS', tempfile.gettempdir())
 INSTALL_DIR = tempfile.gettempdir()
 LOG_FILE = os.path.join(ARTIFACTS_DIR, 'setup-moth.log')
 
+
+def configure_logging(log_file):
+    logging.basicConfig(filename=log_file,
+                        format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                        datefmt='%m-%d %H:%M',
+                        filemode='w',
+                        level=logging.DEBUG)
+
+    # define a Handler which writes INFO messages or higher to the sys.stderr
+    console = ColorLog()
+    console.setLevel(logging.INFO)
+    logging.getLogger('').addHandler(console)
+
+
+class ColorLog(logging.Handler):
+    """
+    A class to print colored messages to stdout
+    """
+
+    COLORS = {
+                logging.CRITICAL: red,
+                logging.ERROR: red,
+                logging.WARNING: yellow,
+                logging.INFO: green,
+                logging.DEBUG: lambda x: x,
+              }
+
+    def __init__(self):
+        logging.Handler.__init__(self)
+
+    def usesTime(self):
+        return False
+
+    def emit(self, record):
+        color = self.COLORS.get(record.levelno, lambda x: x)
+        print(color(record.msg))
 
 def get_source_code():
     '''
