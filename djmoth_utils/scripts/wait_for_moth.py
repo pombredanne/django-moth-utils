95df21b85404d633abeb58a897f383aeb6ff613b
Andres Riancho <andres.riancho@gmail.com>
2013-12-10 14:21:23 -0300

    Don't wait if there is a traceback in the log

--- djmoth_utils/scripts/wait_for_moth.py
+++ djmoth_utils/scripts/wait_for_moth.py
@@ -5,6 +5,7 @@ from __future__ import print_function
 import os
 import sys
 import time
+import tempfile
 
 # See: w3af.core.controllers.ci.moth
 FMT = '/tmp/moth-%s.txt'
@@ -12,6 +13,18 @@ HTTP_ADDRESS_FILE = FMT % 'http'
 HTTPS_ADDRESS_FILE = FMT % 'https'
 
 DELTA = 0.5
+
+TRACEBACK = 'Traceback (most recent call last):'
+ARTIFACTS_DIR = os.environ.get('CIRCLE_ARTIFACTS', tempfile.gettempdir())
+DAEMON_START_LOG = os.path.join(ARTIFACTS_DIR, 'setup-moth-nohup.log')
+
+if os.path.exists(DAEMON_START_LOG):
+    daemon_log = file(DAEMON_START_LOG).read()
+
+    if TRACEBACK in daemon_log:
+        print('Traceback found in daemon start log. Moth failed to start.')
+        print(daemon_log)
+        sys.exit(-1)
 
 wait_time = 0
 print('Waiting for moth to start', end='')
